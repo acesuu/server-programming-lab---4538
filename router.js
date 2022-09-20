2028555818c1845cffac6b09e5f77240c08c2609
acesuu <ahnafakib@iut-dhaka.edu>
2022-09-21 00:13:12 +0600

    added form

--- router.js
+++ router.js
@@ -6,5 +6,12 @@ const { getCV } = require("./controllers/CvController");
 const fs = require("fs");
 
 router.get("/", getCV);
+router.get("/userForm", (req, res) => {
+    res.render("userForm"); 
+});
+router.post("/userForm", getCV);
+
+
+
 
 module.exports = router;
